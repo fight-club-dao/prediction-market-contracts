24e5d08a7c13edb9c987dbe1a93a53b1160dd133
Mark Jackson <psudo.random@gmail.com>
2023-03-04 13:01:57 -0700

    adding ability to borrow from huma contract

--- contracts/FundManager.sol
+++ contracts/FundManager.sol
@@ -11,15 +11,18 @@ import "./interfaces/IUniswapV2Pair.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./huma_contracts/BaseCreditPool.sol";
 
 contract FundManager is Ownable {
     using SafeMath for uint256;
 
     address public fundsToken;
     address public predictionMarket;
+    BaseCreditPool public baseCreditPool;
 
-    constructor(address _fundsToken) public Ownable()  {
+    constructor(address _fundsToken, address _lending_pool) public Ownable()  {
         fundsToken = _fundsToken;
+        baseCreditPool = BaseCreditPool(_lending_pool);
     }
 
     function setToken(address _fundsToken) external onlyOwner() {
@@ -34,6 +37,12 @@ contract FundManager is Ownable {
         require(msg.sender == predictionMarket, "only prediction market can request funds");
         require(_amount <= ERC20(fundsToken).balanceOf(address(this)), "not enough funds");
         ERC20(fundsToken).transfer(msg.sender, _amount);
+    }
+
+    function borrowHumaFunds(uint256 _amount) {
+        require(msg.sender == predictionMarket, "only prediction market can request funds");
+        require(_amount <= ERC20(fundsToken).balanceOf(address(this)), "not enough funds");
+        baseCreditPool.drawdown(_amount);
     }
 
     function returnFunds(uint256 _amount) external {
